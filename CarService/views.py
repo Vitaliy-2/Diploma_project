bbb0476fc44d893b0d44a53bc78e608073144a38
Vitaliy <Vitaliy@yandex.ru>
2024-09-14 22:50:26 +0300

    Добавлено два шаблона + футер прикреплен к низу страницы

--- CarService/views.py
+++ CarService/views.py
@@ -7,8 +7,8 @@ from django.views.generic import (
 
 MENU = [
         {'title': 'Главная', 'url': '/', 'alias': 'main'},
-        {'title': 'О сервисе', 'url': '#masters', 'alias': 'about'},
-        {'title': 'Услуги', 'url': '#services', 'alias': True},
+        {'title': 'О сервисе', 'url': '/about/', 'alias': 'about'},
+        {'title': 'Услуги', 'url': '/services/', 'alias': 'services'},
         {'title': 'Отзывы', 'url': '#reviews', 'alias': True},
         {'title': 'Запись', 'url': '#orderForm', 'alias': True},
     ]
@@ -26,4 +26,26 @@ class MainView(TemplateView):
         context = super().get_context_data(**kwargs)
         context.update(get_menu_context())
         context.update({'page_alias': 'main'})
+        return context
+
+
+class AboutView(TemplateView):
+    template_name = "about.html"
+
+    # Расширяем метод. Добавляем контекст ключ - меню.
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(get_menu_context())
+        context.update({'page_alias': 'about'})
+        return context
+
+
+class ServicesView(TemplateView):
+    template_name = "services.html"
+
+    # Расширяем метод. Добавляем контекст ключ - меню.
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(get_menu_context())
+        context.update({'page_alias': 'services'})
         return context
--- STO/urls.py
+++ STO/urls.py
@@ -4,10 +4,14 @@ from django.urls import path
 
 from CarService.views import (
     MainView,
+    AboutView,
+    ServicesView,
 )
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', MainView.as_view(), name='main'),
+    path('about/', AboutView.as_view(), name='about'),
+    path('services/', ServicesView.as_view(), name='services'),
 ]
