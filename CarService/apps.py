9832e47381c08c03fd800a1b591983503f39b6de
Vitaliy <Vitaliy@yandex.ru>
2024-10-12 14:31:07 +0300

    Подключение телеграм бота

--- CarService/apps.py
+++ CarService/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class CarserviceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'CarService'
+    verbose_name = 'Записи клиентов'
+
+    # По готовности приложения будет импортирован сигнал
+    def ready(self):
+        import CarService.signals
--- STO/settings.py
+++ STO/settings.py
@@ -12,7 +12,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 from django.urls import reverse_lazy
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_g!qgzapi(xpa4a7ycki^$hxygue@+7lq44=9cxgw-e#)nq+&j'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -120,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static/'
 
 
@@ -138,3 +141,7 @@ LOGIN_URL = reverse_lazy("login")
 # Используется как значение по умолчанию, если явно не е указано во view
 LOGIN_REDIRECT_URL = 'main'
 LOGOUT_REDIRECT_URL = 'main'
+
+# Конфигурация для отправки уведомлений в Telegram
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+YOUR_PERSONAL_CHAT_ID = os.getenv("YOUR_PERSONAL_CHAT_ID")
